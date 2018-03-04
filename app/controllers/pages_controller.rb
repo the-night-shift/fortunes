63fa11ce214195db4774f75c4ed4bfe1825a7a8e
Brian <riceb53@gmail.com>
2018-03-04 12:11:52 -0600

    make random fortune page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def make_the_fortune
-    render json: {message: 'hello'}
+    # render a random fortune
+    # get a list of fortunes
+    fortunes = ['You will be rich', 'You will wake up tomorrow', 'It will rain somewhere']
+    # pick a random one every time I run the page
+    selected_fortune = fortunes.sample
+    render json: {fortune: selected_fortune}
   end
 end
