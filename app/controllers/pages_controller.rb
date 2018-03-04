5a310c4e9066af26559b9ed8a54b8891b7337094
Brian <riceb53@gmail.com>
2018-03-04 12:28:16 -0600

    99 bottles of beer on the wall

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,26 @@ class PagesController < ApplicationController
     selected_fortune = fortunes.sample
     render json: {fortune: selected_fortune}
   end
+
+  def lottery_numbers
+    the_numbers = []
+    6.times do
+      the_numbers << 1 + rand(60)
+    end
+    render json: {numbers: the_numbers}
+  end
+
+  def page_count
+    render json: {visits: counter}
+  end
+
+  def beers_on_the_wall
+    counter = 99
+    song_lyrics = []
+    99.times do
+      song_lyrics += "#{counter} bottles of beer on the wall."
+      counter -= 1
+    end
+    render json: {lyrics: song_lyrics}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/fortune_url' => 'pages#make_the_fortune'
+  get '/lotto' => 'pages#lottery_numbers'
+  get '/count' => 'pages#page_count'
+  get '/song' => 'pages#beers_on_the_wall'
 end
